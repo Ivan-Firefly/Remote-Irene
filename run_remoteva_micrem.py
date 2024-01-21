548c32b79ec249db01099fd5cbf73c8d4d9a8cf0
Ivan-Firefly <90849921+Ivan-Firefly@users.noreply.github.com>
2024-01-21 16:06:34 +0300

    Update run_remoteva_micrem.py

--- run_remoteva_micrem.py
+++ run_remoteva_micrem.py
@@ -1,11 +1,3 @@
-import argparse
-import os
-import queue
-import sounddevice as sd
-import vosk
-import sys
-
-import requests
 import json
 
 version="1.0"
@@ -21,15 +13,14 @@ ttsFormat = saved_options["ttsFormat"] # "none" (TTS on server) or "saytxt" (TTS
                     # or mix of the params, splitted by , = "none,saytxt" as example
 ttsFormatList = ttsFormat.split(",")
 baseUrl = saved_options["baseUrl"] # server with Irene WEB api
+deviceId=int(saved_options["deviceId"]) # device id
+samplerateMic=int(saved_options["samplerateMic"]) # sampling rate
 
 from urllib.parse import urlparse
 urlparsed = urlparse(baseUrl)
 
 mic_blocked = False
 
-import json
-import os
-import sys
 import asyncio
 import websockets
 import logging
@@ -48,11 +39,11 @@ def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
     loop.call_soon_threadsafe(audio_queue.put_nowait, bytes(indata))
 
-async def run_test():
+async def run_test(deviceId,samplerateMic):
 
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 4000, device=args.device, dtype='int16',
+    with sd.RawInputStream(samplerate=samplerateMic, blocksize = 4000, device= deviceId, dtype='int16',
                            channels=1, callback=callback) as device:
-        conn_url = str(args.uri) + "/wsmic_"+str(args.samplerate)+"_none"
+        conn_url = str('ws://{0}:{1}'.format(urlparsed.hostname,urlparsed.port)) + "/wsmic_"+str(samplerateMic)+"_none"
         # conn_url = str(args.uri) + "/wsmic"
         print('WEB SOCKET conn URL:',conn_url)
         async with websockets.connect(conn_url) as websocket:
@@ -70,36 +61,39 @@ async def run_test():
 
 async def main():
 
-    global args
+    # global args
     global loop
     global audio_queue
 
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('-l', '--list-devices', action='store_true',
-                        help='show list of audio devices and exit')
-    args, remaining = parser.parse_known_args()
-    if args.list_devices:
-        print(sd.query_devices())
-        parser.exit(0)
-    parser = argparse.ArgumentParser(description="ASR Server",
-                                     formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     parents=[parser])
-    parser.add_argument('-u', '--uri', type=str, metavar='URL',
-                        # by default connect to WEBAPI server with PORT+1
-                        help='Server URL', default='ws://{0}:{1}'.format(urlparsed.hostname,urlparsed.port))
-    parser.add_argument('-d', '--device', type=int_or_str,
-                        help='input device (numeric ID or substring)')
-    parser.add_argument('-r', '--samplerate', type=int, help='sampling rate', default=22050)
-    args = parser.parse_args(remaining)
+    # parser = argparse.ArgumentParser(add_help=False)
+    # parser.add_argument('-l', '--list-devices', action='store_true',
+    #                     help='show list of audio devices and exit')
+    # args, remaining = parser.parse_known_args()
+    # if args.list_devices:
+    #     print(sd.query_devices())
+    #     parser.exit(0)
+    # parser = argparse.ArgumentParser(description="ASR Server",
+    #                                  formatter_class=argparse.RawDescriptionHelpFormatter,
+    #                                  parents=[parser])
+    # parser.add_argument('-u', '--uri', type=str, metavar='URL',
+    #                     # by default connect to WEBAPI server with PORT+1
+    #                     help='Server URL', default='ws://{0}:{1}'.format(urlparsed.hostname,urlparsed.port))
+    # parser.add_argument('-d', '--device', type=int_or_str,
+    #                     help='input device (numeric ID or substring)')
+    # parser.add_argument('-r', '--samplerate', type=int, help='sampling rate', default=22050)
+    # args = parser.parse_args(remaining)
+
+
     loop = asyncio.get_running_loop()
     audio_queue = asyncio.Queue()
 
     logging.basicConfig(level=logging.INFO)
-    print("Remote Irene (VOSK EMBEDDED REMOTE recognizer) v{0} started! ttsFormat={1}, baseUrl={2}, speechRecognizerWebsocketUrl={3}".format(version,ttsFormat,baseUrl,args.uri))
+    print('\nAvaliable devices:')
+    print(sd.query_devices())
+    print()
+    print("Remote Irene (VOSK EMBEDDED REMOTE recognizer) v{0} started! ttsFormat={1}, baseUrl={2}, speechRecognizerWebsocketUrl={3}".format(version,ttsFormat,baseUrl,baseUrl))
 
-    await run_test()
+    await run_test(deviceId,samplerateMic)
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
