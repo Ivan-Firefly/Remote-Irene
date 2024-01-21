87e76bb1cb29248f9cc9281b391e4327e8135bb5
Ivan-Firefly <90849921+Ivan-Firefly@users.noreply.github.com>
2024-01-21 15:02:06 +0300

    Update play_wav.py

--- play_wav.py
+++ play_wav.py
@@ -1,9 +1,10 @@
-import os
+import soundfile as sf
+import sounddevice as sd
 
-from audioplayer import AudioPlayer
-
-def play_wav(wavfile):
-    AudioPlayer(wavfile).play(block=True)
+def play_wav(file_path):
+    data, samplerate = sf.read(file_path)
+    sd.play(data, samplerate)
+    sd.wait()
 
 def saywav_to_file(saywav_result,wavfile):
     import save_wav
